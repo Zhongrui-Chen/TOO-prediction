b1de25289c20e354ff8dd41688d91dfcb7190342
Zhongrui-Chen <zhongrui@proton.me>
2022-06-30 20:13:17 +1000

    include genomic HGVS when making the dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,22 +1,25 @@
-# -------------------------------------------------------------
+# -----------------------------------------------------------------
 # Make the dataset by processing the COSMIC mutation TSV file
 # Steps:
 #   1. Filter and cleanse the dataframe
 #   2. Make the dataset by constructing an aggregative dictionary
 #   3. Pickle the dataset dict and store it in the disk
-# -------------------------------------------------------------
+# -----------------------------------------------------------------
 
 import pickle
 from tqdm import tqdm
 import pandas as pd
 import hgvs.parser
-from src.utils.hgvs_parsing import is_hgvs_valid
+from src.utils.hgvs_parsing import is_hgvs_valid, parse_chrom
 
 # Define the interested columns and primary sites
 cols_of_interest = ['Gene name', 'ID_sample', 'Primary site', 'Tier', 'HGVSC', 'HGVSG']
 sites_of_interest = [
     'kidney', 'skin', 'liver', 'breast', 'ovary', 'haematopoietic_and_lymphoid_tissue',
     'prostate', 'pancreas', 'central_nervous_system', 'lung', 'oesophagus', 'thyroid', 'bone' ]
+
+# Instantiate the hgvs parser
+hp = hgvs.parser.Parser()
 
 def data_filtering(df):
     '''
@@ -32,7 +35,7 @@ def data_filtering(df):
 
     # Filter by primary sites
     len_before = len(df)
-    df = df[df['Primary site'].isin(sites_of_interest)]    
+    df = df[df['Primary site'].isin(sites_of_interest)]
     print('{} rows with non-interested primary sites were removed'.format(len_before - len(df)))
     
     return df
@@ -41,28 +44,26 @@ def data_cleansing(df):
     '''
     Cleanse the dataset by removing unhelpful rows
     '''
-    # Instantiate the hgvs parser
-    hp = hgvs.parser.Parser()
-
     # Remove duplicates
-    # len_before_removing_duplicates = len(df)
-    # df = df.drop_duplicates(subset=['ID_sample', hgvs_type])
-    # print('{} duplicate rows were removed'.format(len_before_removing_duplicates - len(df)))
+    len_before = len(df)
+    df = df.drop_duplicates(subset=['ID_sample', 'HGVSC'])
+    print('{} duplicate rows are removed'.format(len_before - len(df)))
 
-    # Get indices of rows with N/A variant information
+    # Remove rows with N/A variant information
     NaNs = df['HGVSC'].isna()
     idcs = [idx for idx in range(len(df)) if not NaNs.iloc[idx]]
-    print('{} rows with N/A variant information will be removed'.format(len(df) - len(idcs)))
+    print('{} rows with N/A variant information are removed'.format(len(df) - len(idcs)))
+    df = df.iloc[idcs, :]
 
-    # Get indices of rows with non-coding variants
+    # Remove rows with non-coding variants
     print('Removing rows with non-coding variants')
     len_before = len(idcs)
-    for idx in tqdm(idcs):
-        if not is_hgvs_valid(str(df['HGVSC'].iloc[idx]), hp):
-            idcs.remove(idx)
-    print('{} rows with non-coding variants will be removed'.format(len_before - len(idcs)))
-
-    # Remove the rows
+    idcs = []
+    for idx, hgvsc in enumerate(tqdm(df['HGVSC'])):
+        var = hp.parse_hgvs_variant(hgvsc)
+        if is_hgvs_valid(var):
+            idcs.append(idx)
+    print('{} rows with non-coding variants are removed'.format(len_before - len(idcs)))
     df = df.iloc[idcs, :]
 
     return df
@@ -79,26 +80,38 @@ def make_dataset(df, all_sample_ids, all_genes):
     loc_of = {} # Location of a column
     for col in cols_of_interest:
         loc_of[col] = df.columns.get_loc(col)
-    mut_dict = {} # (sample_id, gene) -> [mutations]
+    cmut_dict = {} # (sample_id, gene) -> [mutations]
+    gmut_dict = {} # (sample_id, chromosome) -> [mutations]
     primary_site_dict = {} # sample_id -> primary_site
     for row in df.itertuples(index=False):
         sample_id = row[loc_of['ID_sample']]
         gene = row[loc_of['Gene name']]
-        mut = row[loc_of['HGVSC']]
+        cmut = row[loc_of['HGVSC']]
+        gmut = 'chr' + row[loc_of['HGVSG']] # Add a prefix to make the parser work
+        gvar = hp.parse_hgvs_variant(gmut)
         primary_site = row[loc_of['Primary site']]
+        # Add primary site
         if sample_id not in primary_site_dict:
             primary_site_dict[sample_id] = primary_site
-        if (sample_id, gene) not in mut_dict:
-            mut_dict[(sample_id, gene)] = [mut]
+        # Add coding mutations
+        if (sample_id, gene) not in cmut_dict:
+            cmut_dict[(sample_id, gene)] = [cmut]
         else:
-            mut_dict[(sample_id, gene)].append(mut)
+            cmut_dict[(sample_id, gene)].append(cmut)
+        # Add genomic mutations
+        chrom = parse_chrom(gvar)
+        if (sample_id, chrom) not in gmut_dict:
+            gmut_dict[(sample_id, chrom)] = [gmut]
+        else:
+            gmut_dict[(sample_id, chrom)].append(gmut)
     dataset = {
         # Summary info
         'sample_ids': all_sample_ids,
         'genes': all_genes,
         'primary_sites': sites_of_interest,
         # Sample mappings
-        'mut_dict': mut_dict,
+        'cmut_dict': cmut_dict,
+        'gmut_dict': gmut_dict,
         'primary_site_dict': primary_site_dict
     }
     return dataset
@@ -106,18 +119,24 @@ def make_dataset(df, all_sample_ids, all_genes):
 def main():
     # Read in the COSMIC data as dataframe
     df = pd.read_csv('./data/raw/CosmicMutantExportCensus.tsv', delimiter='\t', encoding='ISO-8859-1')
+    # df = df.sample(n=32) # FIXME
+
     # Filter and cleanse the raw dataframe
-    print('Data filtering...')
+    print('[Data filtering...]')
     df = data_filtering(df)
-    print('Data cleansing...')
+    print('[Data cleansing...]')
     df = data_cleansing(df)
+
     # Get the summary info
     print('There are {} primary sites in total'.format(len(sites_of_interest)))
     all_sample_ids, all_genes = get_summary_info(df)
     print('There are {} samples in total'.format(len(all_sample_ids)))
     print('There are {} genes in total'.format(len(all_genes)))
+
     # Make the dataset
     dataset = make_dataset(df, all_sample_ids, all_genes)
+    # print(dataset['gmut_dict']) # FIXME
+
     with open('./data/interim/dataset.pkl', 'wb') as f:
         pickle.dump(dataset, f)
 
